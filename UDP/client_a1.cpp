199af0f51094258863420b63ada14e92a2803d4b
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-02-12 16:24:42 +0000

    UDP_1

---
+++ UDP/client_a1.cpp
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <iostream>
+
+int main()
+{
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+
+    uint32_t addr = 0x7B628D03;
+    sockaddr_in saddr = {AF_INET, htons(4000), {addr}};
+
+    while(1)
+    {
+        uint32_t val = 1;
+        sendto(s, &val, sizeof(val), 0, (sockaddr*)&saddr, sizeof(saddr));
+
+        uint32_t state;
+        recvfrom(s, &state, sizeof(state), 0, nullptr, nullptr);
+
+        std::cout << "State = " << state << '\n';
+    }
+
+}
---
+++ UDP/server_a1.cpp
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <iostream>
+
+int main()
+{
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+
+    sockaddr_in laddr{AF_INET, htons(4000), {INADDR_ANY}};
+    bind(s, (const sockaddr*)&laddr, sizeof(laddr));
+
+    uint32_t state = 0;
+
+    while(1)
+    {
+        sock_addr_in raddr;
+        socklen_t raddr_len=sizeof(raddr)
+        uint32_t val;
+        recvfrom(s, &val, sizeof(val), 0, (sockaddr*)&raddr, &raddr_len);
+
+        state += val;
+
+        sendto(s, &state, sizeof(state), 0, (const sockaddr*)&raddr, raddr_len);
+        
+    }
+
+
+}
