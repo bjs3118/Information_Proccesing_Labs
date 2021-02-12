8be0e1ebe985907427c3b811d79709107684cc55
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-02-12 15:01:45 +0000

    a_3

---
+++ client_a3.cpp
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <iostream>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+    sockaddr_in saddr = {AF_INET, htons(4000), {0x7B628D03}};
+
+    while(1)
+    {
+        int s = socket(AF_INET, SOCK_STREAM, 0);
+
+        connect(s, (sockaddr*)&saddr, sizeof(saddr));
+
+        uint32_t value = 1;
+        send(s, &value, 4, 0);
+
+        uint32_t payload;
+        recv(s, &payload, 4, 0);
+
+        std::cout << "state = " << payload << '\n';
+
+        close(s);
+    }
+}
---
+++ server_a3.cpp
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <iostream>
+
+int main()
+{
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in laddr{AF_INET, htons(4000), {0}};
+    bind(s, (sockaddr*)&laddr, sizeof(laddr));
+
+    listen(s, 16);
+
+    uint32_t accumulator =0;
+    while(1){
+        int client = accept(s, nullptr, nullptr);
+
+        uint32_t payload;
+        recv(client, &payload, 4, 0);
+
+        accumulator += payload;
+
+        send(client, &accumulator, 4, 0);
+
+        close(client)
+    }
+
+}
