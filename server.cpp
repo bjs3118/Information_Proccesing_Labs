86df0ca7fe3245ec80021ef17c025c3e90c1b4f8
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-02-12 14:48:04 +0000

    s

--- server.cpp
+++ server.cpp
@@ -7,30 +7,20 @@
 #define PORT 8080 
 int main(int argc, char const *argv[]) 
 { 
-    int server_fd, new_socket, valread; 
+    int s, new_socket, valread; 
     struct sockaddr_in address; 
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
     int hello = 69;
     // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
         std::cerr << "socket failed" << '\n'; 
     } 
        
     // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-       std::cerr << ("setsockopt") << '\n';
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
+    if (bind(s, (const sockaddr *)&address, sizeof(address))<0) 
     { 
         std::cerr << "bind failed" << '\n';  
     } 
@@ -38,16 +28,22 @@ int main(int argc, char const *argv[])
     { 
         std::cerr << ("listen") << '\n'; 
     } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        std::cerr << "accept" << '\n'; 
-    } 
-    valread = read( new_socket , buffer, 1024); 
+    
+    uint32_t accumulator = 0;
 
-    std::cerr << buffer << '\n'; 
-    send(new_socket , hello , strlen(hello) , 0 ); 
+    while(1) 
+    {
+        int client = accept(s, nullptr, nullptr)
 
-    std::cout << "Hello message sent" << '\n' ; 
+        uint32_t payload;
+        recv(client, &payload, 4, 0);
+
+        accumulator += payload;
+
+        cout << "New accumulator value = " << accumulator << endl;
+
+        send(client, &accumulator, 4, 0);
+
+        close(client);
     return 0; 
 } 
