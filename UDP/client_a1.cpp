2b7cba54447ec854ab7213df207b9d88684936b8
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-02-13 23:26:00 +0000

    UDP-3

--- UDP/client_a1.cpp
+++ UDP/client_a1.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int s = socket(AF_INET, SOCK_DGRAM, 0);
 
-    uint32_t addr = 0x3e528503;
+    uint32_t addr = 0x03e528503;
     //0x4000a8c0;
     sockaddr_in saddr = {AF_INET, htons(4000), {addr}};
 
